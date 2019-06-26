e7ac70af1b5ba8f120517358add8f42bc9a51cec
axionos <axionoso@gmail.com>
2019-06-26 10:56:41 -0400

    seed dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User
-tim = User.find_or_create_by(username: "tim", password: "tim")
+# User.all
+tim = User.find_or_create_by(username: "tim", password_digest: "tim")
 
 # Trip
-summer = Trip.find_or_create_by(title: "Summer Vacation", start: "2019, 8, 10", end: "2019, 8, 20", user_id: 1 )
-winter = Trip.find_or_create_by(title: "Winter Vacation", start: "2019, 12, 10", end: "2019, 12, 20", user_id: 1 )
+# Trip.all # User.all.first.trips
+summer = Trip.find_or_create_by(title: "Summer Vacation", start: "2019-8-10", end: "2019-8-20", user_id: 1 )
+winter = Trip.find_or_create_by(title: "Winter Vacation", start: "2019-12-10", end: "2019-12-20", user_id: 1 )
 
 # Day
-day1 = Day.find_or_create_by(trip_id: 1)
-day2 = Day.find_or_create_by(trip_id: 1)
-day3 = Day.find_or_create_by(trip_id: 1)
+# Day.all # User.all.first.trips.first.days
+day1 = Day.create(trip_id: 1)
+day2 = Day.create(trip_id: 1)
+day3 = Day.create(trip_id: 1)
 
 # Item
+#Item.all # Day.all # User.all.first.trips.first.days.first.items
 tower = Item.find_or_create_by(place: "Eiffel Tower", memo: "it is a famous landmark of the city", day_id: 1)
 museum = Item.find_or_create_by(place: "Louvre Museum", memo: "it is a famous museum of the city", day_id: 1)
-shopping = Item.find_or_create_by(place: "Champs-Élysées", memo: "it is a famous shopping street of the city", day_id: 2)
+shopping = Item.find_or_create_by(place: "Champs-Élysées", memo: "it is a famous shopping street of the city", day_id: 3)
 
 puts "Seeding Completed"
